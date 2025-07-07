96aba5e6aa6639f80ef79f31afbd0188c76f3527
kaif-0 <kaif8700979251@gmail.com>
2025-07-07 18:24:17 +0530

    fix: exported as function

--- server.js
+++ server.js
@@ -81,6 +81,4 @@ app.post("/getResponse", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
