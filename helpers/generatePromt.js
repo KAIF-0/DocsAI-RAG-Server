a62a6a95b35063e1e545cf0a4a5c060bdcfc6a43
kaif-0 <kaif8700979251@gmail.com>
2025-07-17 00:08:44 +0530

    fix: prompt optimization for out of context query

--- helpers/generatePromt.js
+++ helpers/generatePromt.js
@@ -1,8 +1,14 @@
-export const generatePrompt = (query, url, key, context) => {
-  console.log(context);
+export const generatePrompt = (query, url, key, context = "") => {
+  console.log(context)
   return `You are an expert documentation assistant.
 
-    Instructions:
+Documentation Site Name: ${key}  
+Documentation Site URL: ${url}  
+User Question: "${query}"
+
+- Context: "${context}"
+
+Instructions:
 - If the question is clearly not related to the documentation site name or url, strictly respond with:
   > "Sorry, I cannot answer non-related questions!"
 - If the user's question is clearly related to this documentation site or its content, provide the most accurate, helpful, and detailed answer based on the context below.
@@ -13,12 +19,5 @@ export const generatePrompt = (query, url, key, context) => {
   - Potential edge cases
   - Gotchas to avoid
 - Always include at least one **code example** (even if the user didn't request it), as long as it makes sense for the answer.
-
-
-Documentation Site Name: ${key}  
-Documentation Site URL: ${url}  
-User Question: "${query}"
-
-- Context: "${context}"
 `;
 };
